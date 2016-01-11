481beba7e544e09023e9dedc743a69e0b45382e2
TriLe965 <lequangminhtri@gmail.com>
2016-01-11 00:49:41 +0000

    add config-mode

--- py/filter_rg_gvf.py
+++ py/filter_rg_gvf.py
@@ -1,163 +1,153 @@
 #!/usr/bin/env python
-#
-#
-USAGE = '''
-USAGE (all parameters are OPTIONAL):
-    -size\ta:b / a: / :b\tRange of variant size
-    -chr\tn1 n2 ...\tTargeted chromosome
-    -count\ta:b / a: / :b\tRange of variant count
-    -range\ta:b / a: / :b\tRange of searching position
-    -type\tvar_type1 var_type2 ...\tFor a list variant type, please find it in our manual
-    -output\toutput_file\tDefault is "filtered_output.gvf")
-\n\n
-'''
-
-####
-from sys import argv
 import os
+import sys
 import ConfigParser
+import time
 import glob
-####
 
+#database = ['VarType=complex_dbVar.gvf']
+database = glob.glob('/home/ubuntu/gvf_by_studies/by_studies/*/*.gvf')
+config_header_filter = 'filter'
 
-#### Getting database
-database = glob.glob('/home/ubuntu/dbvar_data/output/*.gvf')
-#print database
-####
+def get_parameter(key_word,data_type,default):
+	cmd = sys.argv
+	output = data_type()
+	if '-config' in cmd:
+		config_file = cmd[cmd.index('-config')+1]
+		config = ConfigParser.ConfigParser()
+		config.read(config_file)
+		try:
+			output = config.get(config_header_filter,key_word)
+			if data_type == list:
+                                output = output.split()
+                        else:
+                                output = data_type(output)
+		except ConfigParser.NoSectionError:
+			print '\nContiguration file is in wrong format\n'
+			exit()
+		except ConfigParser.NoOptionError:
+			output = data_type()
+	elif '-'+key_word in cmd:
+		output = data_type()
+		for i in range(cmd.index('-'+key_word)+1,len(cmd)):
+			if cmd[i][0]=='-':
+				break
+			elif data_type == list:
+				output.append(cmd[i])
+			else:
+				output += data_type(cmd[i])
 
+	if output == data_type():
+		print 'Set ['+key_word+'] as default'
+		return default
+	else:
+		print 'Set ['+key_word+'] as '+str(output)
+		return output
 
-####
-def check_chr(base,var):
-    if base == []:
-        return True
-    var = var[3:]
-    if var.lower() in base or var.upper() in base:
-        return True
-    else:
-        return False
+def get_range(string):
+	string = string.split(':')
+	if string[0]=='':
+		start = 0
+	else:
+		start = int(string[0])
+	if string[1]=='':
+		end = float('inf')
+	else:
+		end = int(string[1])
+	return [start,end]
 
-def check_count(base,var):
-    var = int(var.split(';')[1].split('=')[-1])
-    base = base.split(':')
-    if base == ['']:
-        return True
+def standardize_list(a_list,data_type=str):
+	for i in range(len(a_list)):
+		if data_type==str:
+			a_list[i]==a_list[i].lower()
+		else:
+			a_list[i]==data_type(a_list[i])
+	return a_list
 
-    if base[0]=='':
-        base[0]=0
-    if base[1]=='':
-        base[1]='inf'
+def check_range(search_range,line):
+	line = line.split()
+	if int(line[3])>search_range[1]:
+		return 'FalseWithBreak'
+	elif int(line[3])<search_range[0] or int(line[4])>search_range[1]:
+		return False
+	else:
+		return True
 
-    base[0]=float(base[0])
-    base[1]=float(base[1])
-    #print base,var
+def check_count(search_count,line):
+	line = line.split()[8].split(';')
+	for item in line:
+		if 'count' in item:
+			count = int(item.replace('\n','').split('=')[-1])
+			break
+	if count<search_count[0] or count>search_count[1]:
+		return False
+	else:
+		return True
 
-    if var>base[1] or var<base[0]:
-        return False
-    else:
-        return True
-
-def check_type(base,var):
-    if base == []:
-        return True
-
-    for item in base:
-        if item.lower()==var.lower():
+def check_type(search_type,line):
+	var_type = line.split()[2]
+        if search_type == 'all':
             return True
-    return False
-            
-def check_region(base_region,var_region):
-    base_region = base_region.split(':')
-    if base_region == ['']:
-        return True
+	if var_type in search_type:
+		return True
+	else:
+		return False
 
-    if base_region[0]=='':
-        base_region[0]=0
-    if base_region[1]=='':
-        base_region[1]='inf'
-    base_start,var_start=float(base_region[0]),float(var_region[0])
-    base_end,var_end=float(base_region[1]),float(var_region[1])
-    if var_start>base_region:
-        return 'FalseWithBreak'
-    elif var_start<base_start or var_end>base_end:
-        return False
-    else:
-        return True
+def check_chr(search_chr,line):
+	var_chr = line.split()[0][3:]
+	if search_chr == ['all']:
+		return True
+	elif var_chr in search_chr:
+		return True
+	else:
+		return False
 
-def get_data(key,data_type,default=None):
-    #Get data from command line
-    if key not in argv:
-        if default == None:
-            return data_type()
-        else:
-            return default
+def check_len(search_len,line):
+	line = line.split()
+	var_len = int(line[4])-int(line[3])+1
+	if var_len<search_len[0] or var_len>search_len[1]:
+		return False
+	else:
+		return True
 
-    key_in = argv.index(key)
-    if data_type==str:
-        output = argv[key_in+1]
-    elif data_type==list:
-        output = []
-        for i in range(key_in+1,len(argv)):
-            if argv[i][0]=='-':
-                break
-            else:
-                output.append(argv[i])
-    return output
-
-
-def check_size(base,var):
-    base = base.split(':')
-    if base==['']:
-        return True
-
-    if base[0]=='':
-        base[0]=0
-    if base[1]=='':
-        base[0]='inf'
-    base[0]=float(base[0])
-    base[1]=float(base[1])
-    var = int(var[1])-int(var[0])+1
-
-    if var>base[1] or var<base[0]:
-        return False
-    else:
-        return True
+def standardize_dir(a_dir):
+	if a_dir[-1]=='/':
+		a_dir = a_dir[:-1]
+	return a_dir
 
 def main():
-    config = ConfigParser.RawConfigParser()
-    if '-h' in argv:
-        print USAGE
-        exit()
-    output_file = get_data('-output',str,'filtered_output.gvf')
-    base_region = get_data('-range',str)
-    var_type = get_data('-type',list)
-    base_count = get_data('-count',str)
-    base_chr = get_data('-chr',list)
-    base_size = get_data('-size',str)
-    if output_file == None:
-        output_file = os.path.join( 
-                config.get('output', 'output_dir'), 
-                'filtered_output.gvf')
-
-
-    save = open(output_file,'w')
-    for data in database:
-        read = open(data)
-        for Line in read:
-            if Line[0]!='#':
-                line = Line.split('\t')
-                var_region = line[3:5]
-                key1 = check_region(base_region,var_region)
-                key2 = check_type(var_type,line[2])
-                key3 = check_count(base_count,line[8])
-                key4 = check_chr(base_chr,line[0])
-                key5 = check_size(base_size,var_region)
-                keys = [key1,key2,key3,key4,key5]
-
-                if 'FalseWithBreak' in keys:
-                    break
-                elif False not in keys:
-                    save.write(Line)
-    save.close()
-####
-
-main()
+	print
+	#Get parameters
+        start = time.time()
+	search_range = get_range(get_parameter('range',str,':'))
+	search_count = get_range(get_parameter('count',str,':'))
+	search_chr = standardize_list(get_parameter('chr',list,['all']))
+	search_type = standardize_list(get_parameter('type',list,'all'))
+	search_len = get_range(get_parameter('size',str,':'))
+	output_dir = standardize_dir(get_parameter('output',str,os.getcwd()))
+	#Initilize
+	try:
+		os.makedirs(output_dir)
+		print 'Create new directory',output_dir
+	except OSError:
+		print 'Overwrite files in directory:',output_dir
+        filtered_data = open('/'.join([output_dir,'filtered_data.gvf']),'w')
+	#Filter
+        print '\nFiltering...'
+	for data in database:
+		data = open(data)
+		for line in data:
+			if line[0]!='#':
+				status = []
+				status.append(check_range(search_range,line))
+				status.append(check_type(search_type,line))
+				status.append(check_count(search_count,line))
+				status.append(check_chr(search_chr,line))
+				status.append(check_len(search_len,line))
+				if 'FalseWithBreak' in status:
+					break
+				elif False not in status:
+					filtered_data.write(line)
+        print 'Done! Running time: '+str(round(time.time()-start,2))+'s\n'
+if __name__ == '__main__':
+	main()
