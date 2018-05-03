949a8dfee31ece179f1c7eae82f4ac3047f154e0
John Garner <JohnG-001@users.noreply.github.com>
2018-05-03 14:27:43 -0400

    Update download_data.py
    Changed tab to tsv to reflect changes in dbVar directory and file structure

--- py/download_data.py
+++ py/download_data.py
@@ -15,20 +15,20 @@ def main():
 	f.cwd('pub/dbVar/data/Homo_sapiens/by_study/')
 	ldir = list()
 	f.retrlines('NLST',ldir.append)
-	f.cwd(ldir[0]+'/tab')
+	f.cwd(ldir[0]+'/tsv')
 	for dir in ldir[1:]:
 		ldir2 = list()
 		#f.retrlines('NLST',ldir2.append)
-		#f.cwd(dir+'/tab')
+		#f.cwd(dir+'/tsv')
 		f.retrlines('NLST',ldir2.append)
 		for dir2 in ldir2:
 			#print dir3,type(dir3)
-			if 'germline.tab.gz' in dir2 and 'variant_call.' in dir2:
+			if 'germline.tsv.gz' in dir2 and 'variant_call.' in dir2:
 				print 'downloading',dir2
 				f2 = open(dir2,'wb')
 				f.retrbinary('RETR '+dir2,f2.write)
 		#f.cwd('../')
-		f.cwd('../../'+dir+'/tab')
+		f.cwd('../../'+dir+'/tsv')
 	print 'Done'
 
 main()
